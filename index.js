91b019c7d86289259d9814ce3a8157b089badc06
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-01-29 18:38:22 -0300

    Deploy BackEnd

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+const cors = require("cors");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+app.use(cors()); 
+
+let items = [
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+];
+
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor");
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Obtener todos los elementos
+app.get("/api/items", (req, res) => {
+  res.json(items);
+});
+
+// Agregar un elemento nuevo
+app.post("/api/items", (req, res) => {
+  const newItem = req.body;
+  //busco el último id para asignar uno nuevo al elemento que agrego
+  const lastItemId = items.length > 0 ? items[items.length - 1].id : 0;
+  newItem.id = lastItemId + 1;
+  items.push(newItem);
+  res.json(newItem);
+});
+
+// Actualizar un elemento existente
+app.put("/api/items/:id", (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const updatedItem = req.body;
+  items = items.map((item) =>
+    item.id === itemId ? { ...item, ...updatedItem } : item
+  );
+  res.json(updatedItem);
+});
+
+// Eliminar un elemento
+app.delete("/api/items/:id", (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const itemToRemove = items.find((item) => item.id === itemId);
+  if (!itemToRemove) {
+    return res.status(404).json({ message: "Elemento no encontrado" });
+  }
+  items = items.filter((item) => item.id !== itemId);
+  res.json({ message: "Item eliminado" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
